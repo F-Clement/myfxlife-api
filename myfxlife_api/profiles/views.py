11f60d4e7ff8a78b1e555278299d60e9d7a522d6
Clement Fonyuy <fonyuyclement@gmail.com>
2024-02-13 11:03:00 +0000

    Add serializer, permissions write views and connect urls for profiles

--- myfxlife_api/profiles/views.py
+++ myfxlife_api/profiles/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from myfxlife_api.permissions import IsOwnerOrReadOnly
+from .models import Profile
+from .serializers import ProfileSerializer
 
-# Create your views here.
+
+class ProfileList(generics.ListCreateAPIView):
+    """
+    Using generic views to create to achieve get and post functionality
+    """
+    serializer_class = ProfileSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Profile.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
