432d6dee4204b6863600cdc68e15ed0e3468399d
F-Clement <fonyuyclement@gmail.com>
2024-02-15 12:44:46 +0100

    Add image filters and validation for post images then some finishing touches before deploying

--- myfxlife_api/posts/models.py
+++ myfxlife_api/posts/models.py
@@ -6,6 +6,16 @@ from django.contrib.auth.models import User
 
 
 class Post(models.Model):
+    #Image filter choices when user uploads image
+    image_filter_choices = [
+    ('_1977', '1977'), ('brannan', 'Brannan'),
+    ('earlybird', 'Earlybird'), ('hudson', 'Hudson'),
+    ('inkwell', 'Inkwell'), ('lofi', 'Lo-Fi'),
+    ('kelvin', 'Kelvin'), ('normal', 'Normal'),
+    ('nashville', 'Nashville'), ('rise', 'Rise'),
+    ('toaster', 'Toaster'), ('valencia', 'Valencia'),
+    ('walden', 'Walden'), ('xpro2', 'X-pro II')
+]
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
@@ -13,6 +23,9 @@ class Post(models.Model):
     content = models.TextField(blank=True)
     image = models.ImageField(
         upload_to='images/', default='../default_post_bi81gn', blank=True
+    )
+    image_filter = models.CharField(
+        max_length= 32, choices = image_filter_choices , default = 'normal'
     )
 
     class Meta:
--- myfxlife_api/posts/serializers.py
+++ myfxlife_api/posts/serializers.py
@@ -15,6 +15,20 @@ class PostSerializer(serializers.ModelSerializer):
     comments_count = serializers.ReadOnlyField()
     likes_count = serializers.ReadOnlyField()
 
+
+    def validate_image(self, value):
+        if value.size > 2 * 1024 * 1024:
+            raise serializers.ValidationError('Image size larger than 2MB!')
+        if value.image.height > 4096:
+            raise serializers.ValidationError(
+                'Image height larger than 4096px!'
+            )
+        if value.image.width > 4096:
+            raise serializers.ValidationError(
+                'Image width larger than 4096px!'
+            )
+        return value
+
     def get_is_owner(self, obj):
         request = self.context['request']
         return request.user == obj.owner
@@ -34,5 +48,5 @@ class PostSerializer(serializers.ModelSerializer):
         fields = [
             'id', 'owner', 'is_owner', 'profile_id',
             'profile_image', 'created_at', 'updated_at',
-            'title', 'content', 'image', 'like_id', 'comments_count', 'likes_count'
+            'title', 'content', 'image', 'image_filter', 'like_id', 'comments_count', 'likes_count'
         ]
