7f2e1924f898fc506d58705817365c16d6101ae0
F-Clement <fonyuyclement@gmail.com>
2024-02-19 10:41:22 +0100

    Update allowed host

--- myfxlife_api/settings.py
+++ myfxlife_api/settings.py
@@ -66,7 +66,7 @@ DEBUG = 'DEV' in os.environ
 
 
 ALLOWED_HOSTS = [
-   os.environ.get('ALLOWED_HOST'),'http://127.0.0.1:8000'
+   os.environ.get('ALLOWED_HOST'),'http://127.0.0.1:8000', '127.0.0.1'
 ]
 
 # Application definition
